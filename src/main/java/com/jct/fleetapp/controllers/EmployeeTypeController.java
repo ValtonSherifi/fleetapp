db353918dd12978695793b5c6ab9bf4b6fecaeaa
Valton <valtoni.gl@live.com>
2021-04-18 17:54:01 +0200

    linking the dashboard with pages through controllers

--- src/main/java/com/jct/fleetapp/controllers/EmployeeTypeController.java
+++ src/main/java/com/jct/fleetapp/controllers/EmployeeTypeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class EmployeeTypeController {
 
-	@GetMapping("/employeestype")
+	@GetMapping("/employees-type")
 	public String getEmployeesType() {
 		return "employeetype";
 	}
