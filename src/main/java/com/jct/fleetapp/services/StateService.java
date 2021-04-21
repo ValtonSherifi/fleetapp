153db521c5989a014edf41c27c7052912ec1e5c3
Valton <valtoni.gl@live.com>
2021-04-21 15:54:51 +0200

    CRUD operations for States

--- src/main/java/com/jct/fleetapp/services/StateService.java
+++ src/main/java/com/jct/fleetapp/services/StateService.java
@@ -1,8 +1,41 @@
 package com.jct.fleetapp.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.jct.fleetapp.models.State;
+import com.jct.fleetapp.models.State;
+import com.jct.fleetapp.repositories.StateRepository;
+import com.jct.fleetapp.repositories.StateRepository;
 
 @Service
 public class StateService {
 
+
+	@Autowired
+	private StateRepository stateRepository;
+	
+	//return all the states
+	public List<State> getStates(){
+		return stateRepository.findAll();
+	}
+	
+	//save/add new state
+	
+	public void save(State state) {
+		stateRepository.save(state);
+	}
+	
+	public Optional<State> findById(int id) {
+		return stateRepository.findById(id);
+	}
+
+	public void delete(int id) {
+		 stateRepository.deleteById(id);
+	}
+
+	
 }
