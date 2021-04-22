f01732914e43e328d72e1c2252467f4708ae8195
Valton <valtoni.gl@live.com>
2021-04-22 21:19:14 +0200

    Update EmployeeTypeController.java

--- src/main/java/com/jct/fleetapp/controllers/EmployeeTypeController.java
+++ src/main/java/com/jct/fleetapp/controllers/EmployeeTypeController.java
@@ -1,13 +1,64 @@
 package com.jct.fleetapp.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.jct.fleetapp.models.Country;
+import com.jct.fleetapp.models.Location;
+import com.jct.fleetapp.models.State;
+import com.jct.fleetapp.models.EmployeeType;
+import com.jct.fleetapp.services.StateService;
+import com.jct.fleetapp.services.EmployeeTypeService;
 
 @Controller
 public class EmployeeTypeController {
 
-	@GetMapping("/employees-type")
-	public String getEmployeesType() {
-		return "employeetype";
+	@Autowired
+	private EmployeeTypeService employeeTypeService;
+
+	@GetMapping("/employeeTypes")
+	public String getEmployeeTypes(Model model) {
+
+		List<EmployeeType> employeeTypeList = employeeTypeService.getEmployeeTypes();
+		
+		model.addAttribute("employeeTypes", employeeTypeList);
+		
+		return "EmployeeType";
+	}
+
+	@PostMapping("/employeeTypes/addNew")
+	public String addNew(EmployeeType employeeType) {
+		employeeTypeService.save(employeeType);
+
+		return "redirect:/employeeTypes";
+	}
+
+	@RequestMapping("/employeeTypes/findById")
+	@ResponseBody
+	public Optional<EmployeeType> findById(int id) {
+		return employeeTypeService.findById(id);
+	}
+
+	@RequestMapping(value = "/employeeTypes/update", method = { RequestMethod.PUT, RequestMethod.GET })
+	public String update(EmployeeType employeeType) {
+
+		employeeTypeService.save(employeeType);
+
+		return "redirect:/employeeTypes";
+	}
+
+	@RequestMapping(value = "/employeeTypes/delete", method = { RequestMethod.DELETE, RequestMethod.GET })
+	public String delete(int id) {
+		employeeTypeService.delete(id);
+		return "redirect:/employeeTypes";
 	}
 }
