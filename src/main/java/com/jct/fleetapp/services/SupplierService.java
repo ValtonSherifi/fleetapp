199ca675ad0c6c74e6344174e4c194b26a77e1ea
Valton <valtoni.gl@live.com>
2021-04-23 18:30:32 +0200

    Supplier page completed

--- src/main/java/com/jct/fleetapp/services/SupplierService.java
+++ src/main/java/com/jct/fleetapp/services/SupplierService.java
@@ -1,8 +1,37 @@
 package com.jct.fleetapp.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.jct.fleetapp.models.Supplier;
+import com.jct.fleetapp.repositories.SupplierRepository;
 
 @Service
 public class SupplierService {
+
+	@Autowired
+	private SupplierRepository supplierRepository;
+
+	public List<Supplier> getSuppliers() {
+
+		return supplierRepository.findAll();
+	}
+
+	public void save(Supplier supplier) {
+
+		supplierRepository.save(supplier);
+	}
+
+	public Optional<Supplier> findById(int id) {
+		
+		return supplierRepository.findById(id);
+	}
+	
+	public void delete(int id) {
+		supplierRepository.deleteById(id);
+	}
 
 }
