09db4302ba88b85358e4cd1de929877530435e19
Valton <valtoni.gl@live.com>
2021-04-24 12:19:26 +0200

    vMaintenanceService and VehicleService methods

--- src/main/java/com/jct/fleetapp/services/VehicleMaintenanceService.java
+++ src/main/java/com/jct/fleetapp/services/VehicleMaintenanceService.java
@@ -1,8 +1,38 @@
 package com.jct.fleetapp.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.jct.fleetapp.models.VehicleMaintenance;
+import com.jct.fleetapp.repositories.VehicleMaintenanceRepository;
+import com.jct.fleetapp.repositories.VehicleMakeRepository;
 
 @Service
 public class VehicleMaintenanceService {
+
+	@Autowired
+	private VehicleMaintenanceRepository vehicleMaintenanceRepository;
+
+	public List<VehicleMaintenance> getVehicleMaintenances() {
+		
+		return vehicleMaintenanceRepository.findAll();
+	}
+	
+	public void save(VehicleMaintenance vehicleMaintenance) {
+		
+		vehicleMaintenanceRepository.save(vehicleMaintenance);
+	}
+	
+	public Optional<VehicleMaintenance> findById(int id) {
+		
+		return vehicleMaintenanceRepository.findById(id);
+	}
+	
+	public void delete(int id) {
+		vehicleMaintenanceRepository.deleteById(id);
+	}
 
 }
--- src/main/java/com/jct/fleetapp/services/VehicleService.java
+++ src/main/java/com/jct/fleetapp/services/VehicleService.java
@@ -1,8 +1,36 @@
 package com.jct.fleetapp.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.jct.fleetapp.models.Vehicle;
+import com.jct.fleetapp.repositories.VehicleRepository;
 
 @Service
 public class VehicleService {
+
+	@Autowired
+	private VehicleRepository vehicleRepository;
+
+	public List<Vehicle> getVehicles() {
+		
+		return vehicleRepository.findAll();
+	}
+
+	public void save(Vehicle vehicle) {
+		
+		vehicleRepository.save(vehicle);
+	}
+	
+	public Optional<Vehicle> findById(int id) {
+		return vehicleRepository.findById(id);
+	}
+	
+	public void delete(int id) {
+		vehicleRepository.deleteById(id);
+	}
 
 }
