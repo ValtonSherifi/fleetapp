239caa3af38c70840a59619c0d7fe1ddd147e703
Valton <valtoni.gl@live.com>
2021-04-26 16:07:27 +0200

    Vehicle, VehicleHire and VehicleMovement completed

--- src/main/java/com/jct/fleetapp/services/VehicleHireService.java
+++ src/main/java/com/jct/fleetapp/services/VehicleHireService.java
@@ -1,8 +1,38 @@
 package com.jct.fleetapp.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.jct.fleetapp.models.VehicleHire;
+import com.jct.fleetapp.repositories.VehicleHireRepository;
 
 @Service
 public class VehicleHireService {
+
+	@Autowired
+	private VehicleHireRepository vehicleHireRepository;
+
+	public List<VehicleHire> getVehicleHires() {
+		
+		return vehicleHireRepository.findAll();
+	}
+	
+	public Optional<VehicleHire> findById(int id) {
+		
+		return vehicleHireRepository.findById(id);
+	}
+	
+	public void save(VehicleHire vehicleHire) {
+		
+		vehicleHireRepository.save(vehicleHire);
+	}
+	
+	public void delete(int id) {
+		
+		vehicleHireRepository.deleteById(id);
+	}
 
 }
--- src/main/java/com/jct/fleetapp/services/VehicleMovementService.java
+++ src/main/java/com/jct/fleetapp/services/VehicleMovementService.java
@@ -1,8 +1,38 @@
 package com.jct.fleetapp.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.jct.fleetapp.models.VehicleMovement;
+import com.jct.fleetapp.repositories.VehicleMovementRepository;
 
 @Service
 public class VehicleMovementService {
 
+	@Autowired
+	VehicleMovementRepository vehicleMovementRepository;
+	
+
+	public List<VehicleMovement> getVehicleMovements() {
+
+		return vehicleMovementRepository.findAll();
+	}
+
+	public void save(VehicleMovement vehicleMovement) {
+
+		vehicleMovementRepository.save(vehicleMovement);
+	}
+
+	public Optional<VehicleMovement> findById(int id) {
+
+		return vehicleMovementRepository.findById(id);
+	}
+	
+	public void delete(int id) {
+		
+		vehicleMovementRepository.deleteById(id);
+	}
 }
