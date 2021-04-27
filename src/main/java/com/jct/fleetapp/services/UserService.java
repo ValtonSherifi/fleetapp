42015778e6ccf985c6265217da874a35ece0988b
Valton <valtoni.gl@live.com>
2021-04-27 16:25:42 +0200

    User registration

--- src/main/java/com/jct/fleetapp/services/UserService.java
+++ src/main/java/com/jct/fleetapp/services/UserService.java
@@ -1,8 +1,19 @@
 package com.jct.fleetapp.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.jct.fleetapp.models.User;
+import com.jct.fleetapp.repositories.UserRepository;
 
 @Service
 public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	public void save(User user) {
+		userRepository.save(user);
+	}
 
 }
